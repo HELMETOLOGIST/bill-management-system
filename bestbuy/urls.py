59acf3676563305f86ccab5005edc4754d5dd999
Nijith <nijithcv@gmail.com>
2024-07-26 01:49:01 +0530

    product add delete list completed

--- bestbuy/urls.py
+++ bestbuy/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('',views.store_loginn,name='store_login'),
-    path('store_dashboard/',views.store_dashboardd,name='store_dashboard'),
-    path('store_billing/',views.store_billingg,name='store_billing'),
-    path('store_items/',views.store_itemss,name='store_items'),
-    path('store_report/',views.store_reportt,name='store_report'),
-
+    path('', views.store_loginn, name='store_login'),
+    path('store_dashboard/', views.store_dashboardd, name='store_dashboard'),
+    path('store_billing/', views.store_billingg, name='store_billing'),
+    path('store_items/', views.store_itemss, name='store_items'),
+    path('store_items_add/', views.store_items_addd, name='store_items_add'),
+    path('store_items/store_items_edit/<str:id>/', views.store_items_editt, name='store_items_edit'),
+    path('store_items/store_items_delete/<str:id>/', views.store_items_delete, name='store_items_delete'),
+    path('store_report/', views.store_reportt, name='store_report'),
+    path('admin_logout', views.admin_logoutt, name='admin_logout'),
 ]
--- bestbuy/views.py
+++ bestbuy/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as admin_login, logout as admin_logout
 from django.contrib import messages
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import user_passes_test
+from .models import Product
+from .forms import ProductForm
+from django.core.exceptions import ObjectDoesNotExist
+
+
 
 # Create your views here.
 '''
 Admin login logic implementation only
 '''
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def store_loginn(request):
     if request.user.is_authenticated and request.user.is_superuser:
         return redirect('store_dashboard')
@@ -24,24 +31,85 @@ def store_loginn(request):
     else:
         return render(request, 'store_login.html')
 
-
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
 def admin_logoutt(request):
     admin_logout(request)
     return redirect('store_login')
     
 
-
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
 def store_dashboardd(request):
     return render(request, 'store_dashboard.html')
 
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
 def store_billingg(request):
     return render(request, 'store_billing.html')
 
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
 def store_itemss(request):
-    return render(request, 'store_items.html')
+    products = Product.objects.all()
+    return render(request, 'store_items.html', {'products': products})
 
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
+def store_items_addd(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product_name')
+        stock = request.POST.get('stock')
+        price = request.POST.get('price')
+        try:
+            existing_product = Product.objects.get(name__iexact=product_name)
+            messages.error(request, 'This product already added')
+            return redirect("store_items_add")
+        except ObjectDoesNotExist:
+            pass
+
+        product = Product(
+            name=product_name,
+            stock=stock,
+            price=price,
+        )
+        product.save()
+        messages.success(request, 'Product added successfully')
+        return redirect("store_items_add")
+    else:
+        return render(request, 'store_items_add.html')
+    
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
+def store_items_editt(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        edit_name = request.POST.get('product_name')
+        edit_stock = request.POST.get('stock')
+        edit_price = request.POST.get('price')
+
+        product.name = edit_name
+        product.stock = edit_stock
+        product.price = edit_price
+        product.save()
+        return redirect('store_items')
+    return render(request, 'store_items_edit.html', {'product': product})
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
+def store_items_delete(request, id):
+    product = get_object_or_404(Product, id=id)
+    product.delete()
+    messages.success(request, 'Product deleted successfully')
+    return redirect('store_items')
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@user_passes_test(lambda u: u.is_superuser, login_url="store_login")
 def store_reportt(request):
     return render(request, 'store_report.html')
